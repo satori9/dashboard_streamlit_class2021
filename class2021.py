e10e69126c58c0cafc88f06ec18409dce59da62e
satori9 <38586507+satori9@users.noreply.github.com>
2022-05-23 11:31:30 +0900

    Update class2021.py

--- class2021.py
+++ class2021.py
@@ -113,6 +113,22 @@ with col5:
     color = st.color_picker('Q1-5', '#FF58C4')
     st.write(color)
 
+#1選択時に追加で表示（とりあえずテスト。本当は自動化したい）
+if choose_id == '1':
+    col1, col2, col3 = st.columns(3)
+
+with col1:
+    color = st.color_picker('Q1-1', '#FFFFFF')
+    st.write(color)
+
+with col2:
+    color = st.color_picker('Q1_2', '#FFB8E6')
+    st.write(color)
+
+with col3:
+    color = st.color_picker('Q1-3', '#FFE8E8')
+    st.write(color)
+
     
 #!楽しい   
 df_tanoshii = df[df['kwd'] == '(2) 楽しい']
