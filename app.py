cd0ea7c48307c4de296c389e9ddb61e59852fad6
satori9 <38586507+satori9@users.noreply.github.com>
2022-05-28 17:46:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
 page.app()
+
+app = MultiApp()
+app.add_app("Result", class2021.app)
+app.add_app("Analysis", all2021.app)
+app.run()
