27b978c21c7092a571ba7e1ea3fa3090ce2437d9
satori9 <38586507+satori9@users.noreply.github.com>
2022-05-28 17:46:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,7 @@ from multiapp import MultiApp
 import class2021
 import all2021
 
-PAGES = {
-    "Result": class2021,
-    "Analysis": all2021
-}
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+
 
 app = MultiApp()
 app.add_app("Result", class2021.app)
