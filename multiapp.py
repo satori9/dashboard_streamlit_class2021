9d85a8a417be617bf266cd1f2dacfb99be09113d
satori9 <38586507+satori9@users.noreply.github.com>
2022-05-29 14:22:05 +0900

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -5,15 +5,35 @@ class MultiApp:
         self.apps = []
 
     def add_app(self, title, func):
-        self.apps.append({
-            "title": title,
-            "function": func
-        })
+        self.apps.append({"title": title, "function": func})
 
+    #def run(self):
+     #   app = st.sidebar.radio(
+     #       'Go To',
+     #       self.apps,
+     #       format_func=lambda app: app['title'])
+
+    #    app['function']()
+        
     def run(self):
-        app = st.sidebar.radio(
-            'Go To',
-            self.apps,
-            format_func=lambda app: app['title'])
+        app_state = st.experimental_get_query_params()
+        app_state = {
+            k: v[0] if isinstance(v, list) else v for k, v in app_state.items()
+        }  # fetch the first item in each query string as we don't have multiple values for each query string key in this example
 
-        app['function']()
+        # st.write('before', app_state)
+
+        titles = [a["title"] for a in self.apps]
+        functions = [a["function"] for a in self.apps]
+        default_radio = titles.index(app_state["page"]) if "page" in app_state else 0
+
+        st.sidebar.title("Navigation")
+
+        title = st.sidebar.radio("Go To", titles, index=default_radio, key="radio")
+
+        app_state["page"] = st.session_state.radio
+        # st.write('after', app_state)
+
+        st.experimental_set_query_params(**app_state)
+        # st.experimental_set_query_params(**st.session_state.to_dict())
+        functions[titles.index(title)]()
