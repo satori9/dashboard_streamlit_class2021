a1f5f827219eab44f45c98df1a81032c5b757f22
satori9 <38586507+satori9@users.noreply.github.com>
2022-05-29 15:21:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from apps import (class2021,  all2021)
 apps = MultiApp()
 
 # Add all your application here
-apps.add_app("Result", class2021.app)
-apps.add_app("Analysis", all2021.app)
+apps.add_app("結果", class2021.app)
+apps.add_app("分析", all2021.app)
 
 # The main app
 apps.run()
